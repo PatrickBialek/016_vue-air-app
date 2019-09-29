21a060edc2e0513b94b13ddb9616b83f0b0d2970
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-09-29 21:14:57 +0200

    Add example test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,11 +1,15 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import {
+  shallowMount
+} from '@vue/test-utils'
+import Home from '@/components/Home/Home.vue'
 
-describe('HelloWorld.vue', () => {
+describe('Home.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+    const wrapper = shallowMount(Home, {
+      propsData: {
+        msg
+      }
     })
     expect(wrapper.text()).toMatch(msg)
   })
